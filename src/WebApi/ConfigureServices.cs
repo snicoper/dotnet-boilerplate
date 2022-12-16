f04457ae657a3e05e395bc93d6ba719629a2f8f6
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-16 08:55:14 +0100

    Configure WebApi project

---
+++ src/WebApi/ConfigureServices.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Generation.Processors.Security;
+using NSwag;
+
+namespace Dotnetboilerpla.WebApi;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebUIServices(this IServiceCollection services)
+    {
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+        services.AddHttpContextAccessor();
+
+        services.AddHealthChecks()
+            .AddDbContextCheck<ApplicationDbContext>();
+
+        services.AddControllersWithViews(options =>
+            options.Filters.Add<ApiExceptionFilterAttribute>())
+                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+
+        services.AddRazorPages();
+
+        // Customize default API behavior
+        services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+
+        services.AddOpenApiDocument(configure =>
+        {
+            configure.Title = "JsonTaylor API";
+            configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+            {
+                Type = OpenApiSecuritySchemeType.ApiKey,
+                Name = "Authorization",
+                In = OpenApiSecurityApiKeyLocation.Header,
+                Description = "Type into the textbox: Bearer {your JWT token}."
+            });
+
+            configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+        });
+
+        return services;
+    }
+}
---
+++ src/WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetBolerplate.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private ISender _mediator = null!;
+
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
