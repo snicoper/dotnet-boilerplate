194229a7bd4ce2405f01931c3574805d5c001745
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-17 11:56:14 +0100

    Fix string?

--- src/Application/Common/Interfaces/IIdentityService.cs
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -4,7 +4,7 @@ namespace DotnetBoilerplate.Application.Common.Interfaces;
 
 public interface IIdentityService
 {
-    Task<string> GetUserNameAsync(string userId);
+    Task<string?> GetUserNameAsync(string userId);
 
     Task<bool> IsInRoleAsync(string userId, string role);
 
